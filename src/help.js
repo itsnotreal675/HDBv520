1cfdb203b939abb91e096e6926e5f72b1528988e
itsnotreal675 <76721666+itsnotreal675@users.noreply.github.com>
2020-12-29 23:52:30 -0300

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,25 @@
+const help = () => {
+	return '> ```Comandos Figurinhas HDB BOTv4.0🤡😳``` <\n' +
+	'comando : *!sticker* ou *!stiker*\n' +
+	'descrição : converta imagem/gif/video para figurinha\n' +
+	'como usar : marque a imagem/gif/video, ou envie a imagem/gif/video com a legenda descrita acima\n\n' + 
+	'comando : *!toimg*\n' +
+	'descrição : converta figurinhas para imagens\n' +
+	'como usar : marque a figurinha com a legenda descrita acima\n\n' +
+	'comando : *!tsticker* ou *!tstiker*\n' +
+	'descrição : converta texto para figurinha\n' +
+	'como usar : *!tsticker seu texto aqui*\n\n' +
+	'> ```Meme Comandos``` <\n' +
+	'comando : *!meme*\n' +
+	'descrição : memes gringos aleatório [english]\n' +
+	'como usar: basta enviar o comando\n\n' +
+	'comando : *!memeindo*\n' +
+	'descrição : memes aleatório [indo]\n' +
+	'como usar : basta enviar o comando\n\n' +
+	'> ```Comando Texto para Fala``` <\n' +
+	'comando : *!gtts*\n' +
+	'descrição : converta texto para/audio\n' +
+	'como usar : *!gtts [cc] [text]*\nexample : *!gtts pt alexandre cadê meu adm*'
+}
+
+exports.help = help
